81e4c5b343555f69ccce1906c48d82252760c21c
andre ferrer <andrerferrer@hotmail.com>
2023-03-21 15:06:08 -0300

    declare the type

--- app/graphql/types/restaurant_type.rb
+++ app/graphql/types/restaurant_type.rb
@@ -2,7 +2,10 @@
 
 module Types
   class RestaurantType < Types::BaseObject
-    field :title, String
+    description 'A Restaurant' # we can describe the object
+    field :id, ID, null: false
+    field :title, String,
+      description: 'the title of the resturant' # we can also describe the fields
     field :rating, Integer
   end
 end
