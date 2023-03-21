8722726b16064ed3501652da14c0534c9576d799
andre ferrer <andrerferrer@hotmail.com>
2023-03-21 15:06:13 -0300

    build the schema

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,11 +7,12 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    # FIRST we describe the field signature:
+    field :restaurant, RestaurantType, 'Find a Restaurant', null: false do
+      argument :id, ID
+    end # SECOND we provide an implementation
+    def restaurant(id:)
+      Restaurant.find id
     end
   end
 end
